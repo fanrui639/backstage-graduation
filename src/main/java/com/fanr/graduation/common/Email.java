7d41c888d89e006e5d92356fa118762128330f61
fanr639 <2578543184@qq.com>
2020-03-26 17:23:06 +0800

    解决数据库连接八小时的BUG

--- src/main/java/com/fanr/graduation/common/Email.java
+++ src/main/java/com/fanr/graduation/common/Email.java
@@ -2,13 +2,9 @@ package com.fanr.graduation.common;
 
 import com.sun.mail.util.MailSSLSocketFactory;
 
-import javax.activation.DataHandler;
-import javax.activation.FileDataSource;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 import java.security.GeneralSecurityException;
 import java.util.Properties;
 
